e35e3618c85a44e96cb6cd87595eea14ff15001c
Elijah Schow <elijah.schow@gmail.com>
2017-01-13 17:13:12 -0600

    Implement `stringifyJSON`

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,54 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	var result = "";
+	var property;
+	var item;
+	var i;
+
+	switch(typeof obj){
+		case 'function':
+			result = "";
+		case 'string':
+			result = '"' + obj + '"';
+		break;
+		case 'object':
+			if(obj === null){
+				result = 'null';
+			} else if(obj.constructor === Array){
+				result += '['
+				for(i = 0; i < obj.length; i++){
+					if(typeof obj[i] !== 'undefined' && typeof obj[i] !== 'function'){
+						result += stringifyJSON(obj[i]) + ',';
+					} else {
+						//censor undefined and function values
+						result += 'null,';
+					}
+				}
+				//remove trailing commas
+				if(result[result.length-1] === ","){
+					result = result.slice(0,-1);
+				}
+				result += ']';
+			} else {
+				result += '{';
+				for(property in obj){
+					if(obj.hasOwnProperty(property) && typeof obj[property] !== 'undefined' && typeof obj[property] !== 'function'){
+						//censor undefined and function values
+						result += '"' + property + '":' + stringifyJSON(obj[property]) + ",";
+					}
+				}
+				//remove trailing commas
+				if(result[result.length-1] === ","){
+					result = result.slice(0,-1);
+				}
+				result += "}";
+			}
+		break;
+		default:
+			//Boolean, Number, Undefined, NaN
+			result = String(obj);
+	}
+
+	return result;
 };
